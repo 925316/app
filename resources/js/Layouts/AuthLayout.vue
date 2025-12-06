c40846c4e2707f1060f5cdd760f650d84839b3fd
Bela Proinsias <belaskk@outlook.com>
2025-12-06 21:47:58 +0800

    init

---
+++ resources/js/Layouts/AuthLayout.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { computed } from 'vue';
+import { usePage } from '@inertiajs/vue3';
+
+defineProps({
+    title: String,
+});
+
+const page = usePage();
+</script>
+
+<template>
+    <div class="auth-wrapper">
+        <div class="auth-content">
+            <slot />
+        </div>
+    </div>
+</template>
